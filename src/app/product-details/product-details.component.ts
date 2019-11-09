63383b203b66b81eaee8569125a908e0f927d55b
WuYouyan <wuyouyan1@gmail.com>
2019-11-09 21:32:31 +0100

    inject CartService and create 'Buy' button

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,14 +13,20 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
-  constructor(private route : ActivatedRoute) { 
-    
-  }
+  constructor(
+    private route : ActivatedRoute,
+    private cartService : CartService
+  ) {  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params=>{
       this.product = products[+params.get('productId')] //+params.get('productId') transform string Id to number Id
     });
+  }
+    
+  addToCart(product){
+    window.alert('Your prroduct has been added to the cart !');
+    this.cartService.addToCart(product); 
   }
 
 }
