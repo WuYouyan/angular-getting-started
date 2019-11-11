b30d6d44cc3147a166710646089f1b9f89f17db9
WuYouyan <wuyouyan1@gmail.com>
2019-11-11 15:24:25 +0100

    comments

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';//HttpClient service is provided by the HttpClientModule, which is already defined in the AppModule
 
 @Injectable({
   providedIn: 'root'
