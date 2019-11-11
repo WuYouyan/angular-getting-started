dabba54a9b7f128e8f0dfd5ca761757b418519b8
WuYouyan <wuyouyan1@gmail.com>
2019-11-11 15:22:21 +0100

    Form in the cart built

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms'; //FormBuilder service is provided by the ReactiveFormsModule, which is already defined in the AppModule
+
 
 import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-cart',
@@ -10,12 +13,29 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) { 
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) { 
     this.items = cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name:'',
+      address:''
+    });
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(customerData){
+    //Process checkout data here
+    console.log('Your order has been submitted', customerData);
+
+    this.items= this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
   // remove(item){
